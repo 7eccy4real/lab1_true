ab5f976c59dc523a5bf6d91aa47e23a35209b6e8
Arthur Gabbasov <jecxylay@bk.ru>
2024-03-05 17:30:35 +0300

    работа на уроке

--- lab1_true.cpp
+++ lab1_true.cpp
@@ -1,118 +1,160 @@
-﻿#include <iostream>
-
-class cashRegister {
+﻿class cashRegister
+{
 private:
-    int cashOnHand;
+	int cashOnHand;
 public:
-    cashRegister() {
-        cashOnHand = 500;
-    }
-
-    cashRegister(int cash) {
-        cashOnHand = cash;
-    }
-
-    int getCurrentBalance() {
-        return cashOnHand;
-    }
-
-    void acceptAmount(int amount) {
-        cashOnHand += amount;
-    }
+	cashRegister(); //Конструктор Устанавливает сумму наличных в кассе по умолчанию.
+	cashRegister(int cashIn); //Конструктор Устанавливает наличные в кассе на определенную сумму.
+	int getCurrentBalance();	//Возвращается значение CashOnHand.
+	void acceptAmount(int amountIn);//Функция получения суммы, внесенной клиентом
+	//и обновить сумму в реестре.
 };
 
-class dispenserType {
+// Этот класс определяет элементы для реализации товаров.
+
+
+class dispenserType
+{
 private:
-    int numberOfItems;
-    int typeCost;
+	int numberOfItems; //переменная для хранения количества предметов в магазине
+	int cost; //переменная для хранения стоимости товара
 public:
-    dispenserType() {
-        numberOfItems = 50;
-        typeCost = 50; // Установим стоимость продукта по умолчанию в 50
-    }
-
-    dispenserType(int items, int cost) {
-        numberOfItems = items;
-        typeCost = cost;
-    }
-
-    int getNoOfItems() {
-        return numberOfItems;
-    }
-
-    int getCost() {
-        return typeCost;
-    }
-
-    void makeSale() {
-        if (numberOfItems > 0) {
-            numberOfItems--;
-        }
-        else {
-            std::cout << "Извините, товар закончился." << std::endl;
-        }
-    }
+	dispenserType(); //Конструктор Устанавливает стоимость и количество предметов в раздатчике по умолчанию.
+	dispenserType(int setNoOfItems, int setCost); //Конструктор Устанавливает указанные значения
+	int getNoOfItems();	//Возвращается значение NumberOfItems.
+	int getCost();	//Функция отображения стоимости товара. Возвращается значение стоимости.
+	void makeSale(); //Функция уменьшения количества элементов на 1.
 };
 
-void showSelection() {
-    std::cout << "1. Конфеты" << std::endl;
-    std::cout << "2. Чипсы" << std::endl;
-    std::cout << "3. Жвачка" << std::endl;
-    std::cout << "4. Печенье" << std::endl;
+
+cashRegister::cashRegister()
+{
+	cashOnHand = 500;
 }
 
-void sellProduct(dispenserType& dispenser, cashRegister& cashReg) {
-    int choice;
-    std::cout << "Сделайте выбор: ";
-    std::cin >> choice;
-
-    switch (choice) {
-    case 1:
-        std::cout << "Вы выбрали конфеты." << std::endl;
-        break;
-    case 2:
-        std::cout << "Вы выбрали чипсы." << std::endl;
-        break;
-    case 3:
-        std::cout << "Вы выбрали жвачку." << std::endl;
-        break;
-    case 4:
-        std::cout << "Вы выбрали печенье." << std::endl;
-        break;
-    default:
-        std::cout << "Неверный выбор." << std::endl;
-        return;
-    }
-
-    if (dispenser.getNoOfItems() > 0) {
-        std::cout << "Стоимость товара: " << dispenser.getCost() << std::endl;
-        int amount;
-        std::cout << "Введите сумму: ";
-        std::cin >> amount;
-
-        if (amount >= dispenser.getCost()) {
-            dispenser.makeSale();
-            cashReg.acceptAmount(dispenser.getCost());
-            std::cout << "Спасибо за покупку! Возьмите ваш товар." << std::endl;
-        }
-        else {
-            std::cout << "Недостаточно средств." << std::endl;
-        }
-    }
-    else {
-        std::cout << "Извините, товар закончился." << std::endl;
-    }
+cashRegister::cashRegister(int cashIn)
+{
+	cashOnHand = cashIn;
 }
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    dispenserType candyDispenser;
-    cashRegister cashReg;
+int cashRegister::getCurrentBalance()
+{
+	return cashOnHand;
+}
 
-    std::cout << "Добро пожаловать! Выберите продукт:" << std::endl;
-    showSelection();
+void cashRegister::acceptAmount(int amountIn)
+{
 
-    sellProduct(candyDispenser, cashReg);
+	cashOnHand = cashOnHand + amountIn;
+}
 
-    return 0;
+dispenserType::dispenserType()
+{
+	numberOfItems = 50;
+	cost = 50;
+}
+
+dispenserType::dispenserType(int setNoOfItems, int setCost)
+{
+	numberOfItems = setNoOfItems;
+	cost = setCost;
+}
+
+int dispenserType::getNoOfItems()
+{
+	return numberOfItems;
+}
+
+int dispenserType::getCost()
+{
+	return cost;
+}
+
+void dispenserType::makeSale()
+{
+	numberOfItems--;
+}
+
+#include <iostream>
+
+void showSelection();
+void sellProduct(dispenserType& product, cashRegister& pCounter);
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cashRegister counter;
+	dispenserType candy(100, 50);
+	dispenserType chips(100, 65);
+	dispenserType gum(75, 45);
+	dispenserType cookies(50, 85);
+	int choice;
+	showSelection();
+	std::cin >> choice;
+	while (choice != 5)
+	{
+		switch (choice)
+		{
+		case 1:
+			sellProduct(candy, counter);
+			break;
+		case 2:
+			sellProduct(chips, counter);
+			break;
+		case 3:
+			sellProduct(gum, counter);
+			break;
+		case 4:
+			sellProduct(cookies, counter);
+			break;
+		default:
+			std::cout << "Неверный выбор" << std::endl;
+		}
+		showSelection();
+		std::cin >> choice;
+	}
+	return 0;
+}
+
+void showSelection()
+{
+	std::cout << "Выберете продукт " << std::endl;
+	std::cout << "1 Конфеты" << std::endl;
+	std::cout << "2 Чипсы" << std::endl;
+	std::cout << "3 Жвачка" << std::endl;
+	std::cout << "4 Печеньки" << std::endl;
+	std::cout << "5 Выйти" << std::endl;
+}
+
+void sellProduct(dispenserType& product, cashRegister& pCounter)
+{
+	int amount; //переменная для хранения введенной суммы
+	int amount2; //переменная для хранения необходимой дополнительной суммы
+	if (product.getNoOfItems() > 0) //если товар не пуст
+	{
+		std::cout << "Внесите " << product.getCost() << std::endl;
+		std::cin >> amount;
+		if (amount < product.getCost())
+		{
+			std::cout << "Внесите еще " << product.getCost() - amount << std::endl;
+			std::cin >> amount2;
+			amount = amount + amount2;
+		}
+
+		if (amount = product.getCost())
+		{
+			pCounter.acceptAmount(amount);
+			product.makeSale();
+			std::cout << "Заберите ваш товар" << std::endl;
+		}
+		if (amount > product.getCost())
+		{
+			pCounter.acceptAmount(amount - product.getCost());
+			product.makeSale();
+			std::cout << "Заберите ваш товар и вашу сдачу в размере" << amount - product.getCost() << std::endl;
+		}
+		else
+			std::cout << "Денег не хватает" << "Заберите ваши деньги." << std::endl;
+	}
+	else
+		std::cout << "Извините, товар закончился" << std::endl;
 }
